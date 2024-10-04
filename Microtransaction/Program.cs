191a25c072bd251abc98f5cd2102919bb243ebdd
Matija Zezelj <matija.zezelj@elev.ga.ntig.se>
2024-10-04 10:39:24 +0200

    Pris

--- Microtransaction/Program.cs
+++ Microtransaction/Program.cs
@@ -1,4 +1,24 @@
-﻿int wallet = 100;
+﻿int wallet = 10000;
+
+while (wallet > 0)
+
+{
+Console.WriteLine($"Du har {wallet} kronor kvar");
+
+//Köpa
+
+Console.WriteLine("Vill du köpa nåt?, om du gör inte då kommer jag behöva ta min pew pew");
+
+Console.WriteLine("1. Pistol (500Kr)");
+Console.WriteLine("2. AR-15 (1000Kr)");
+Console.WriteLine("3. AK-47 (2000kr)");
+
+string köpa = "";
+
+while (köpa != "1" && köpa !="2" && köpa !="3")
+{
+    köpa = Console.ReadLine();
+}
 
 
 
@@ -8,6 +28,10 @@
 
 
 
+
+
+
+}
 
 
 Console.ReadLine();
